79a92732757430259774f8923fc86d5c457cf34d
Панюкова <dima.laskov.ru@mail.ru>
2023-12-09 21:01:19 +0300

    add task 12

---
+++ PyCharm/Tasks/TASK12.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def multiply():
+    product = 1
+    number = int(input("Введите число: "))
+
+    while number != 0:
+        product *= number
+        number = int(input("Введите число: "))
+
+    print(product)
+
+
+if __name__ == '__main__':
+    multiply()
+
+
+
+
+    
