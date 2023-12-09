1db159e63d63c2418b96df9b586156c81c31c92e
Панюкова <dima.laskov.ru@mail.ru>
2023-12-09 19:01:08 +0300

    add task 8

---
+++ PyCharm/Tasks/TASK8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def test():
+    # Ввести целое число.
+    number = int(input("Введите целое число: "))
+
+    # Определить знак числа.
+    if number > 0:
+        positive()
+    elif number < 0:
+        negative()
+
+
+def positive():
+    print("Положительное")
+
+
+def negative():
+    print("Отрицательное")
+
+
+if __name__ == '__main__':
+    test()
+
+
